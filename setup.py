b4defc0cf85e5ed3a025b07688a361ef3ec43e7d
Simon Stanley <simon.stanley@informaticslab.co.uk>
2015-06-23 13:50:37 +0100

    Added setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='webcoverageservice',
+      version='0.1.0',
+      install_requires=["requests >= 2.3.0",
+                        "python-dateutil"],
+      description='Python interface to web coverage services',
+      author='Met Office Informatics Lab',
+      maintainer='Met Office Informatics Lab',
+      url='https://github.com/met-office-lab/ogc-web-services',
+      license='TBC',
+      packages=['webcoverageservice'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+      ]
+     )
